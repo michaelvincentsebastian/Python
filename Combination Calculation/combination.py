94e3b6901fe3032d012b0c0dfcb3f40c823fe77e
Michael Vincent Sebastian Handojo <vincentmichael514@gmail.com>
2025-03-12 07:57:49 +0700

    Combination.py

---
+++ Combination Calculation/combination.py
@@ -0,0 +1,28 @@
+''' 
+Combination Calculation Program
+'''
+def nFactorial(n, factorial):
+    for i in range(1, n+1):
+        factorial = factorial*i
+    return factorial
+    
+def rFactorial(r, factorial):
+    for i in range(1, r+1):
+        factorial = factorial*i
+    return factorial
+        
+def nrFactorial(n, r, factorial):
+    for i in range(1, n-r+1):
+        factorial = factorial*i
+    return factorial
+    
+n = int(input("enter total num of object: "))
+r = int(input("enter total num of choosing: "))
+factorial = 1
+
+nFac = nFactorial(n, factorial)
+rFac = rFactorial(r, factorial)
+nminrFac = nrFactorial(n, r, factorial)
+
+calculate = nFac/(rFac*nminrFac)
+print(calculate)
